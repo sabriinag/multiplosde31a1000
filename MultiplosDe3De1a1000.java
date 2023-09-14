4d0aa8fa176715603fa335d19347ba9457857c8a
Sabrina Gonçalves <goncalvessabrina74@gmail.com>
2023-09-14 19:22:23 -0300

    MultiplosDe3De1a1000

---
+++ MultiplosDe3De1a1000.java
@@ -0,0 +1,8 @@
+public class MultiplosDe3De1a1000 {
+    public static void main(String[] args) {
+        System.out.println(" Multiplos de 3 de 1 a 1000");
+        for (int cont=3; cont <=1000; cont = cont + 3){
+            System.out.print (cont + " ");
+        }
+    }
+}
